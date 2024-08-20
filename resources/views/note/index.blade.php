85929cf6426a2e24961cb45f98f0d0d7ab8942cf
Joel Joby <joeljoby28430@gmail.com>
2024-08-20 22:47:04 +0530

    Controller Model and View created

---
+++ resources/views/note/index.blade.php
@@ -0,0 +1,3 @@
+<div>
+    <h1>Hello Joel</h1>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
+use App\Http\Controllers\NoteController;
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [WelcomeController::class, 'index'])->name('welcome');
+
+Route::get('/notes', [NoteController::class, 'index'])->name('notes.index');
+Route::get('/notes/create', [NoteController::class, 'create'])->name('notes.create');
+Route::post('/notes', [NoteController::class, 'store'])->name('notes.store');
+Route::get('/notes/{id}', [NoteController::class, 'show'])->name('notes.show');
+Route::get('/notes/{id}/edit', [NoteController::class, 'edit'])->name('notes.edit');
+Route::put('/notes/{id}', [NoteController::class, 'update'])->name('notes.update');
+Route::delete('/notes/{id}', [NoteController::class, 'destroy'])->name('notes.destroy');
+
+// Route::resource('note',NoteController::class);  //It Was a substitute for the whole 7 route but unfortunitly it didnt work 
